49d5a005e71ab66ed3ad2572c6457a7d386eb2a1
Mario Beltran <mariobeltran0699@gmail.com>
2022-07-13 23:45:12 -0600

    ✨ add index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const Animal = mongoose.model(
+  "Animal",
+  new mongoose.Schema({
+    tipo: String,
+    estado: String,
+  })
+);
+
+const app = express();
+
+mongoose.connect(
+  "mongodb://mario:password@localhost:27018/miapp?authSource=admin"
+);
+
+app.get("/", async (_req, res) => {
+  console.log("listando...");
+  const animales = await Animal.find();
+  return res.send(animales);
+});
+app.get("/crear", async (_req, res) => {
+  console.log("creando...");
+  await Animal.create({ tipo: "Cochinito", estado: "Activo" });
+  return res.send("ok");
+});
+
+app.listen(3000, () => console.log("listening..."));
